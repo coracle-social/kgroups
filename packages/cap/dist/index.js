2e372fc209a251d66b702e28de4ad605ac25d1bd
Jon Staab <shtaab@gmail.com>
2026-01-16 16:56:57 -0800

    Add license

--- packages/cap/dist/index.js
+++
@@ -1,8 +0,0 @@
-// @nkg/cap - Capability issuance and validation
-// Types
-export * from "./types.js";
-// Capability creation and parsing
-export { createCapabilityGrantEvent, createCapabilityRevocationEvent, createCapabilityDelegationEvent, parseCapabilityGrantEvent, parseRevocationEvent, parseDelegationEvent, capabilityAllowsAction, isCapabilityExpired, eventMatchesRequiredTags, eventHasExcludedTags, } from "./capability.js";
-// Validation
-export { validateCapabilityEvent, validateDelegatedCapability, checkAuthorization, CapabilityStore, } from "./validation.js";
-//# sourceMappingURL=index.js.map
--- packages/relay/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./types.js";
-export { isNip29Event, isModerationEvent, isRelayMetadataEvent, getGroupId, getPreviousRefs, parseGroupEvent, validateNip29Event, createGroupMetadataEvent, createGroupAdminsEvent, createGroupMembersEvent, parseGroupMetadataEvent, parseGroupAdminsEvent, parseGroupMembersEvent, parseModerationEvent, canPerformModerationAction, } from "./nip29.js";
-export { Relay } from "./relay.js";
-//# sourceMappingURL=index.d.ts.map
